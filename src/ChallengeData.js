e66cc772fc188120661013834e61ea97d2d14542
JackDixon019 <13709@coderacademy.edu.au>
2023-09-21 17:13:34 +1000

    added some comments

--- src/ChallengeData.js
+++ src/ChallengeData.js
@@ -3,11 +3,16 @@ const API_KEY = process.env.REACT_APP_API_KEY
 const CHALLENGE_DATA = process.env.REACT_APP_CHALLENGE_DATA
 
 async function challengeData(puuid){
+    // gets user's challenge data and parses to object
     let rawChallengeData = await fetch(API_ROOT + CHALLENGE_DATA + puuid + API_KEY);
     let userChallengeData = await rawChallengeData.json()
 
+    // gets challenge config data and parses to object
     let rawMetaData = await fetch(API_ROOT + "/lol/challenges/v1/challenges/config" + API_KEY)
     let metaData = await rawMetaData.json()
+
+    // This part is done because the challenge data is language agnostic, while the metadata
+    // contains all of the different translations for each challenge. 
     // For each challenge in user's data, finds corresponding challenge in config data (by id)
     // adds the name, description, and tier thresholds to each challenge object. 
     userChallengeData.challenges.forEach(challenge => {
@@ -23,6 +28,7 @@ async function challengeData(puuid){
             }
         });
     })
+    
     return userChallengeData
 };
 
