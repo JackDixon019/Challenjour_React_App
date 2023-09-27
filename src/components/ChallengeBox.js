c99cfbb34ccf4940c977edda2161af1ca9042b97
JackDixon019 <13709@coderacademy.edu.au>
2023-09-27 14:19:35 +1000

    How's my favourite branch doin?

--- src/components/ChallengeBox.js
+++ src/components/ChallengeBox.js
@@ -2,6 +2,7 @@ export default function ChallengeBox(props) {
     let challenge = props.singleChallengeData;
     let className = "challengeBox " + challenge.level;
 
+    // some change for git
     // Only displays the details if the index of the box matches the state of the parent element
     if (props.isActive && props.isActiveCategory) {
         return (
