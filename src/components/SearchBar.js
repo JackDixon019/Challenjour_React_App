449bc362a5c9921429270d63edb6219647d195dd
JackDixon019 <13709@coderacademy.edu.au>
2023-09-30 10:27:03 +1000

    pared extraneous data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,38 +2,42 @@ import { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 
 export default function SearchBar() {
-
-    const {userData, setUserData} = useContext(UserContext)
-    let user = useContext(UserContext)
-    const [text, setText] = useState("")
+    const { setUserData } = useContext(UserContext);
+    const [text, setText] = useState("");
 
     // // text is the value in the search bar
     function handleChange(e) {
         setText(e.target.value);
-
     }
 
     // Prevents refresh on submit, changes searchTerm  to text
     // searchTerm is used to perform the search
     function handleSubmit(e) {
         e.preventDefault();
-        setUserData({name: text})
+        setUserData({ name: text });
     }
 
     // this is here because I got tired of typing lol
     function testSubmit(e) {
         e.preventDefault();
-        setUserData({name: "Dredgen Vale"});
+        setUserData({ name: "Dredgen Vale" });
     }
 
     return (
         <form>
             <label htmlFor="nameSearch">Enter name: </label>
-            <input type="text" name="nameSearch" id="nameSearch" onChange={(e) => handleChange(e)} />
+            <input
+                type="text"
+                name="nameSearch"
+                id="nameSearch"
+                onChange={(e) => handleChange(e)}
+            />
             <button type="submit" id="nameSearchSubmit" onClick={(e) => handleSubmit(e)}>
                 Search
             </button>
-            <button type="submit" id="testSubmit" onClick={(e)=>testSubmit(e)}>Load Test Data </button>
+            <button type="submit" id="testSubmit" onClick={(e) => testSubmit(e)}>
+                Load Test Data{" "}
+            </button>
         </form>
     );
 }
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,16 +3,12 @@ import CategoriesSidebar from "./CategoriesSidebar";
 import { useContext} from "react";
 import ChallengeResults from "./ChallengeResults";
 import ChallengeContext from "../context/ChallengeContext";
-import ActiveIndexContext from "../context/ActiveIndexContext";
 import ActiveCategoryContext from "../context/ActiveCategoryContext";
 
 export default function SearchResults() {
 
-    let {activeIndex} = useContext(ActiveIndexContext)
-
     let challengeData = useContext(ChallengeContext).challengeData
     let activeCategory = useContext(ActiveCategoryContext).activeCategory
-    console.log(activeCategory)
     
     let currentScore = 0;
 
@@ -26,12 +22,8 @@ export default function SearchResults() {
     return (
         <div id="resultsPage">
             <h2>Current Category: {activeCategory}</h2> <h2>Score: {currentScore}</h2>
-            <CategoriesSidebar
-                challengeData={challengeData}
-            />
-            <ChallengeResults
-                activeIndex={activeIndex}
-            />
+            <CategoriesSidebar />
+            <ChallengeResults />
         </div>
     );
 }
